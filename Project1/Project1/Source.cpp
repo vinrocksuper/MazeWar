68e38911d953db31bf839f6903cac7790d0e340c
Vincent <vl5604@rit.edu>
2021-02-19 15:55:13 -0500

    Project 1 near completion

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,68 @@
+#include "pch.h"
+
+int mazeWidth;
+int mazeHeight;
+int** mazeData;
+bool startSet = false;
+int startX;
+int startY;
+bool endSet = false;
+int endX;
+int endY;
+
+
+// Returns string of team members name
+__declspec(dllexport) char* GetTeam() {
+	return (char*)"Vincent Li";
+}
+
+// Sets maze data from main into the dll. Save into a variable in the DLL. Use this for GetData function.
+__declspec(dllexport) void SetMaze(const int** data, int width, int height) {
+	
+	
+
+	mazeWidth = width;
+	mazeHeight = height;
+}
+
+// Gets maze data that was passed in using SetMaze function and the width/height using set arguments
+__declspec(dllexport) int** GetMaze(int& width, int& height) {
+	width = mazeWidth;
+	height = mazeHeight;
+	return mazeData;
+}
+
+// returns next x/y pos to move to. 
+__declspec(dllexport) void GetNextPosition(int& xpos, int& ypos) {
+
+}
+
+// sets starting location for player. Saves the x and y values for starting pos
+__declspec(dllexport) void SetStart(int xpos, int ypos) {
+	startX = xpos;
+	startY = ypos;
+	startSet = true;
+}
+
+// gets the starting position for the player. If x and y havent been set yet, return -1 for both
+__declspec(dllexport) void GetStart(int& xpos, int& ypos) {
+	if (startSet) {
+		xpos = startX;
+		ypos = startY;
+	}
+}
+
+// Sets end pos for player. saves x and y location
+__declspec(dllexport) void SetEnd(int xpos, int ypos) {
+	endX = xpos;
+	endY = ypos;
+	endSet = true;
+}
+
+// Gets the end location for player. Returns saved x/y loc. If not set, return -1 for both.
+__declspec(dllexport) void GetEnd(int& xpos, int& ypos) {
+	if (endSet) {
+		xpos = endX;
+		ypos = endY;
+	}
+}
