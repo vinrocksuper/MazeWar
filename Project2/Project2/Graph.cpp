0141df908437850ce72d1c7c5b3c6ffa26bd7119
Vincent <vl5604@rit.edu>
2021-03-19 13:26:49 -0400

    Cont. work on graph class. Unsure if RemoveVertex is going to work as intended.

--- Project2/Project2/Graph.cpp
+++ Project2/Project2/Graph.cpp
@@ -1,2 +1,46 @@
 ﻿#include "pch.h"
 #include "Graph.h"
+
+
+Graph::Graph(int sX, int sY, int eX, int eY, int w, int h)
+{
+	startX = sX;
+	startY = sY;
+	endX = eX;
+	endY = eY;
+	width = w;
+	height = h;
+	
+	
+}
+
+// On moving into new vertex push it to stack. Mark it as visited.
+void Graph::AddVertex(Vertex* vert)
+{
+	vertStack.push(vert);
+	//vert->visited = true;
+}
+
+void Graph::AssignHeuristic(Vertex* vert)
+{
+	vert->heuristic = (endX - vert->xPos) + (endY - vert->yPos);
+}
+
+// If no adj unvisited vertex, pop it from stack
+// Else push adjacent vertex into stack
+void Graph::RemoveVertex()
+{
+	bool zeroAdj = true;
+	for(int i=0;i < width;i++)
+	{
+		if(adjMatrix[currentVertex->xPos][i] == 1 )
+		{
+			// push adj vertex into stack
+			zeroAdj = false;
+		}
+	}
+	if(zeroAdj)
+	{
+		vertStack.pop();
+	}
+}
--- Project2/Project2/Graph.h
+++ Project2/Project2/Graph.h
@@ -1,18 +1,47 @@
 ﻿#pragma once
 #include <list>
-
+#include <stack>
 #include "Vertex.h"
 
+
+using namespace std;
 class Graph
 {
 public:
 	//List of vertices in graph
-	list<Vertex> verticies;
-	int** adjMatrix;
-	Vertex currentVertex;
+	list<Vertex*> verticies; // Can use Vertex<Vertex*> (note vertex is c++ class not the vertex that we define)
+	int** adjMatrix; //   2D Array of adjacencies
 
+	
+	Vertex* currentVertex; // Try to use ptrs as much as possible
+	stack<Vertex*> vertStack; // Stack of verts (Last In First Out)
+
+	
+	// Take these in from Source.cpp, save it here to reduce amnt of times needed to call in this info.
+	int startX;
+	int startY;
+	int endX;
+	int endY;
+	int width;
+	int height;
+	//Constructors
+	Graph(int sX, int sY, int eX, int eY, int w, int h);
+
+
+
+
+
+	//Methods
+	
 	// Adds vertex into list of vertices
-	void AddVertex(Vertex vert);
+	void AddVertex(Vertex* vert);
 
-	//
+	// If no adjacent unvisited, pop vertex.
+	void RemoveVertex();
+	
+	// Assigns the Heuristic value to the vertex
+	// Using Manhattan Values
+	void AssignHeuristic(Vertex* vert);
+
+	
 };
